f81ff160b8d809433b7493d5e868d397aa2d948f
Kaloomc <kaloo132mc@gmail.com>
2024-10-12 10:04:39 +0200

    credits

--- lang/en/local_resend_password_profile.php
+++ lang/en/local_resend_password_profile.php
@@ -6,4 +6,6 @@ $string['accountnewemail'] = 'Votre nouveau mot de passe est : {$a}';
 $string['emailresent'] = "L'email d'activation a été renvoyé avec succès.";
 $string['emailnotresent'] = "Erreur lors du renvoi de l'email d'activation.";
 
+$string['credits'] = 'Développé par François Garnier';
+
 
--- version.php
+++ version.php
@@ -6,3 +6,9 @@ $plugin->version = 2024101114; // Version du plugin.
 $plugin->requires = 2021051700; // Version minimale de Moodle requise.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.0';
+
+
+$plugin->credits = [
+    'author' => 'François Garnier',
+    'email' => 'francoisjgarnier@icloud.com'
+];
