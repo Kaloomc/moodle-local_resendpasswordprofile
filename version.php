2d4f6bb14add61a0ccb40d1492a166b294c51674
Kaloomc <kaloo132mc@gmail.com>
2024-10-12 09:59:14 +0200

    Commit v1.0

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_resend_password_profile';
+$plugin->version = 2024101114; // Version du plugin.
+$plugin->requires = 2021051700; // Version minimale de Moodle requise.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v1.0';
