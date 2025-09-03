f599b6899fd7ea8628d66417fb938d87b33d9ca2
Kaloomc <kaloo132mc@gmail.com>
2025-09-03 13:02:32 +0200

    moodle fix

--- lib.php
+++ lib.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+defined('MOODLE_INTERNAL') || die();
+
 /**
  * This file contains the code for the plugin integration.
  *
@@ -23,54 +25,45 @@
  */
 
 /**
- * Function local_resend_password_profile_myprofile_navigation
+ * Adds a "Resend password" button to the user's profile navigation
+ * if the viewer is a site administrator.
  *
- * Adds a button to the user's profile navigation if the user is an administrator.
+ * @param core_user\output\myprofile\tree $tree     The navigation tree.
+ * @param stdClass $user                           The user whose profile is being viewed.
+ * @param bool $iscurrentuser                      True if the user is the current user.
+ * @param stdClass|null $course                    The course object if available.
  *
- * @param core_user\output\myprofile\tree $tree         The navigation tree to which the new nodes will be added.
- * @param stdClass $user                                 The user object containing user details.
- * @param bool $iscurrentuser                             True if the user is the current user, false otherwise.
- * @param stdClass|null $course                           The course object if available, null otherwise.
- *
- * @throws coding_exception                                 If there is a coding error.
- * @throws moodle_exception                                 If there is a Moodle related error.
+ * @return void
  */
 function local_resend_password_profile_myprofile_navigation($tree, $user, $iscurrentuser, $course) {
-    // Check if the user is logged in and an administrator.
     if (!isloggedin() || !is_siteadmin()) {
         return;
     }
 
-    // Creates a new category in the user profile.
     $category = new core_user\output\myprofile\category(
         'category',
         get_string('category', 'local_resend_password_profile'),
         null
     );
 
-    // Create the single_button instance for the Resend button.
     $resendbutton = new single_button(
-        new moodle_url('/local/resend_password_profile/resend_email.php', ['userid' => $user->id]),
+        new moodle_url('/local/resend_password_profile/resend_email.php', [
+            'userid' => $user->id,
+            'sesskey' => sesskey()
+        ]),
         get_string('button', 'local_resend_password_profile'),
         'post'
     );
 
-    // Render the single_button to HTML.
     global $OUTPUT;
     $buttonhtml = $OUTPUT->render($resendbutton);
 
-    // Creates a new node with the rendered button HTML.
     $node = new core_user\output\myprofile\node(
         'category',
         'buttonnode',
-        $buttonhtml,
-        null,
-        null
+        $buttonhtml
     );
 
-    // Adds the node to the category.
     $category->add_node($node);
-
-    // Adds the category to the profile tree.
     $tree->add_category($category);
 }
